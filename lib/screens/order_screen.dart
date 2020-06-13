6cea4c1f1d925d250d56bac1287879f2ff964b49
Leman Fotera <lemanfoetra@gmail.com>
2020-06-13 10:26:16 +0700

    Menggunakan 'FutureBuilder'

--- lib/screens/order_screen.dart
+++ lib/screens/order_screen.dart
@@ -3,44 +3,36 @@ import 'package:provider/provider.dart';
 import '../providers/orders_provider.dart' show OrdersProvider;
 import '../widgets/order_item.dart';
 
-class OrdersScreeen extends StatefulWidget {
+class OrdersScreeen extends StatelessWidget {
   static const routeName = '/orders';
 
   @override
-  _OrdersScreeenState createState() => _OrdersScreeenState();
-}
-
-class _OrdersScreeenState extends State<OrdersScreeen> {
-  bool _isload = false;
-  @override
-  void initState() {
-    Future.delayed(Duration.zero).then((_) async {
-      setState(() {
-        _isload = true;
-      });
-      final orderProviderObj =
-          Provider.of<OrdersProvider>(context, listen: false);
-      await orderProviderObj.getOrderData();
-      setState(() {
-        _isload = false;
-      });
-    });
-
-    super.initState();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    var ordersData = Provider.of<OrdersProvider>(context);
 
     return Scaffold(
-      appBar: AppBar(title: Text('Your Order')),
-      body: _isload
-          ? Center(child: CircularProgressIndicator())
-          : ListView.builder(
-              itemCount: ordersData.order.length,
-              itemBuilder: (ctx, i) => OrderItem(ordersData.order[i]),
-            ),
-    );
+        appBar: AppBar(title: Text('Your Order')),
+        body: FutureBuilder(
+          future: Provider.of<OrdersProvider>(context, listen: false)
+              .getOrderData(),
+          builder: (ctx, snapShoot) {
+            if (snapShoot.connectionState == ConnectionState.waiting) {
+              // ketika loading
+              return Center(child: CircularProgressIndicator());
+            } else if (snapShoot.error != null) {
+              // ketika error
+              return Center(
+                child: Text('Terjadi Kesalahan jaringan.'),
+              );
+            } else {
+              // ketika semua baik baik saja
+              return Consumer<OrdersProvider>(
+                builder: (ctx, ordersData, child) => ListView.builder(
+                  itemCount: ordersData.order.length,
+                  itemBuilder: (ctx, i) => OrderItem(ordersData.order[i]),
+                ),
+              );
+            }
+          },
+        ));
   }
 }
